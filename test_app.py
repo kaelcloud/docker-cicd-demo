79a9900f12b09573f3015ccb0a5ec9c0d89959e0
Hazran <hazranhafiz.ahmad@gmail.com>
2026-06-04 03:14:19 +0800

    Correct the pipeline

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import app
 def test_home():
     client = app.test_client()
     response = client.get("/")
-    assert response.status_code == 400
+    assert response.status_code == 200
 
 
 def test_health():
